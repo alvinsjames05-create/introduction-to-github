8b34eb29ce23aba4d13e05f5f9a0cbd5726e5650
james <alvinsjames05@gmail.com>
2026-04-14 08:32:44 +0300

    add blog.html,script.py

---
+++ script.py
@@ -0,0 +1,8 @@
+…or create a new repository on the command line
+echo "# introduction-to-github" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/alvinsjames05-create/introduction-to-github.git
+git push -u origin main
